4f95db30f552c0249cfb6dd323f41d2de6c6a07a
Tilak Gupta <gupta.tilak@hotmail.com>
2024-01-16 21:56:23 -0800

    Throw some notes in the README.md

--- exchange.cpp
+++ exchange.cpp
@@ -546,36 +546,5 @@ int main() {
         }
         order->print();
         e.addOrder(std::move(order));
-    }
-
-    // What is a limit order?
-    // guaranteed to buy/sell at a specific price or better!
-    // How to match bid limit orders? (ask would be inverse)
-    // 1. Against ask market order if market price is lower than limit
-    // 2. Against other ask limit order if ask limit price < bid limit price
-    // Should we match a limit order against a market order or another limit order?
-    // * Depends on which would give the order a better match.   
+    } 
 }
-
-    // Notes on storing orders:
-    // Each security needs its own store of orders
-    // for bid and ask sides
-    
-    // 1. How to store market orders?
-    // No price.
-    // Thus, store in order of arrival.
-    // Vector of unique pointers?
-
-    // 2. How to store limit orders?
-    // Have limit price.
-    // Must store relative to price time priority
-    // Order is not sequential like market orders
-    // Use red-black trees for O(log(n)) insertion and retreival.
-
-    // 3. Matching strategy:
-    // Runs every time the security price is updated
-    // 
-    // 3a. Market order:
-    //     Check if matching order on bid/ask side 
-    //     Perform partial/full match
-    //     Store remaining orders in queue
